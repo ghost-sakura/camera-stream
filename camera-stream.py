e9677d6a7a46212b712c8aa6e09616b16eb4dccd
ghost-sakura <s34463201@gmail.com>
2025-12-15 00:21:40 +0530

    Update camera-stream.py

--- camera-stream.py
+++ camera-stream.py
@@ -1,23 +1,26 @@
-from vidstream import CameraClient
-from vidstream import StreamingServer
-
+from vidstream import CameraClient, StreamingServer
 import threading
 import time
 
-recieving = StreamingServer('192.168.1.8', 9999)
+# Streaming server (runs on the camera sender machine)
+receiving = StreamingServer('0.0.0.0', 9999)
 
-sending = CameraClient('192.168.1.7',9999)
+# Camera client (connects to the server IP)
+sending = CameraClient('SERVER_IPV4_ADDRESS', 9999)
 
-t1 = threading.Thread(target = recieving.start_server)
+# Start server thread
+t1 = threading.Thread(target=receiving.start_server)
 t1.start()
 
 time.sleep(2)
 
-t2 = threading.Thread(target = sending.start_stream)
+# Start client thread
+t2 = threading.Thread(target=sending.start_stream)
 t2.start()
 
-while input("") != "STOP":
-    continue
+# Stop streaming on user input
+while input("Type STOP to end streaming: ") != "STOP":
+    pass
 
-recieving.stop_server()
-sending.stop_stream()   
+receiving.stop_server()
+sending.stop_stream()
